192387bdaef0d135985a11d8ed1e5b4397ec2f43
taffylaffy-bit <trendero@usc.edu>
2026-03-23 14:14:34 -0700

    Updated heatmap

--- Assets/Scripts/Telemetry/HeatMapDrawer.cs
+++ Assets/Scripts/Telemetry/HeatMapDrawer.cs
@@ -1,38 +1,14 @@
 using UnityEngine;
-using System.Collections.Generic;
-using System.IO;
 
-public class HeatMapRecorder : MonoBehaviour
+public class HeatMapDrawer : MonoBehaviour
 {
-    public static List<Vector3> positions = new List<Vector3>();
-    float timer;
+    public GameObject heatDot;
 
-    void Update()
+    void Start()
     {
-        timer += Time.deltaTime;
-
-        if (timer > 1f)
+        /*foreach (Vector3 pos in HeatMapRecorder.positions)
         {
-            positions.Add(transform.position);
-            timer = 0f;
-        }
-    }
-
-    void OnApplicationQuit()
-    {
-        string path = Application.persistentDataPath + "/heatmap.csv";
-
-        StreamWriter writer = new StreamWriter(path);
-
-        writer.WriteLine("x,y");
-
-        foreach (Vector3 pos in positions)
-        {
-            writer.WriteLine(pos.x + "," + pos.y);
-        }
-
-        writer.Close();
-
-        Debug.Log("Heatmap saved to: " + path);
+            Instantiate(heatDot, pos, Quaternion.identity);
+        }*/
     }
 }
