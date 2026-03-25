456ae8f9be7036334443a2494c325beae0cff9e6
isun000 <isabelsun000@gmail.com>
2026-03-25 01:00:14 -0700

    cutscene, footsteps

---
+++ Assets/Destroy.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    public GameObject video;
+
+    void Start()
+    {
+        Destroy(video,28f);
+    }
+
+    void Update()
+    {
+        
+    }
+
+}
